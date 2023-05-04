eab971ca15b89f34b349aa9ff533eec2d979a26f
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2023-05-04 15:19:03 +0800

    WIP QR Scan

--- app/src/main/java/com/example/attendanceapp/TeacherActivity.java
+++ app/src/main/java/com/example/attendanceapp/TeacherActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.ResultReceiver;
 import android.util.Log;
@@ -11,10 +13,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TeacherActivity extends BaseActivity {
+    String local_IP = Constants.LOCAL_IP;
     private Button logoutButton, scanButton;
 
     @Override
@@ -43,7 +62,7 @@ public class TeacherActivity extends BaseActivity {
 
     protected void initiateScan() {
         IntentIntegrator intentIntegrator = new IntentIntegrator(TeacherActivity.this);
-        intentIntegrator.setOrientationLocked(true);
+        intentIntegrator.setOrientationLocked(false);
         intentIntegrator.setPrompt("Scan a QR Code");
         intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
         intentIntegrator.initiateScan();
@@ -54,7 +73,47 @@ public class TeacherActivity extends BaseActivity {
         IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (intentResult != null) {
             String contents = intentResult.getContents();
-            Log.d("Content", contents);
+            Log.d("content", contents);
+            String url = local_IP + "/attendance_app/api/verify_attendance.php";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String status = jsonObject.getString("status");
+                        if (status.equals("success")) {
+                            String message = jsonObject.getString("message");
+                            Log.d("response", message);
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                        } else if (status.equals("error")) {
+                            String message = jsonObject.getString("message");
+                            Log.d("response", message);
+                            // Display error message
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Reopen the QR scanner
+                    initiateScan();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Error getting verification string", Toast.LENGTH_SHORT).show();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("unique_string", contents);
+                    return params;
+                }
+            };
+
+            Volley.newRequestQueue(this).add(stringRequest);
         }
 
         super.onActivityResult(requestCode, resultCode, data);
